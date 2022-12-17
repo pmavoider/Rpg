1292e8c4bc3c42ebe11ef15f76b1dcd733e0b52b
pmavoider <63155677+pmavoider@users.noreply.github.com>
2022-12-17 12:24:42 -0600

    Add files via upload

--- Rpg/explore.py
+++ Rpg/explore.py
@@ -1,128 +1,232 @@
 from tkinter import *
 from tkinter import ttk 
-from rooms import entry
+from PIL import Image, ImageTk
 import random
 
+# Array for room choices 
 rooms =[
     ["Small Room", True], 
     ["Large Room", False], 
     [ "Medium Room", True] 
 ]
-outcomes = [True,True,True,False]
-treasure = [20, 5, 10, 0]
+#array for room adjectives 
+roomAdjectives = ["musty", "dimly lit", "bright", "dirty", "surprisingly clean and well lit" ]
+#array used to randomize encounters
+outcomes = [True,True,False]
+#array to randomize treasures
+treasure = [8, 5, 10, 4, 2]
+#array for npc clues
 npcCues = ["You hear heavy breathing. ", "You hear a shuffling of feet", "you see movement out of the corner of your eye", "you hear a voice in the darkness"]
-traps = ["As you exit you hear a click and watch as spikes shoot out of the wall you are dead."]
-
+# array for different traps 
+traps = ["As you exit you hear a click and watch as spikes shoot out of the wall you are dead.", "The floor slides out from under you and you find yourself falling."]
+# enemies array 
 enemies = [ 
     ["goblin", 1, True ],
+    ["hobgoblin", 3, False],
+    ["centaur" ,5, False],
+    ["centaur", 5 , True], 
+    ["spirit", 3,True],
+    ["spirit", 3, False],
     ["troll", 8, False],
     ["human", 3, True],
     ["human", 4, False]
 ]
-global life
-life = 20
-global gold
-gold = 0
 
 
-curentTreasure = 0 
+
+#Main function 
 def gameplay(name, userClass):
+    # Variable to track users gold count 
     global treasureCount
     treasureCount = 0
-# functions to grey buttons
+    global life
+    life = 20
+
+
+# function disables movement buttons and activates interaction buttons 
     def interaction():
         leaveButton.config(state=DISABLED)
         exploreButton.config(state=DISABLED)
         speakButton.config(state=NORMAL)
         fightButton.config(state=NORMAL)
 
+    # function enables movement buttons and disables interaction buttons
     def nonInteraction():
         leaveButton.config(state=NORMAL)
         exploreButton.config(state=NORMAL)
         speakButton.config(state=DISABLED)
         fightButton.config(state=DISABLED)
+    
 
-   
+
+    #function allows user to explore enviroment 
     def explore():
         global treasureCount
         if random.choice(outcomes):
 
             found = random.choice(treasure)
-            header["text"] = "You found " + str(found) + " gold Pieces."
+            header["text"] = "You found " + str(found) + " treasure."
             treasureCount += found
         else:
             header["text"] = random.choice(npcCues)
             interaction()
         update()
+        exploreButton.config(state=DISABLED)
 
+    # function to control speaking with npc's
     def speak():
         global life
         global treasureCount
         character = random.choice(enemies)
-        print(character[2])
         if random.choice(outcomes):
             if random.choice(outcomes):
                 header["text"] = "The " + character[0] +  " attacks you, After a small skirmish you slay the " + character[0]+ ". You gain " + str(character[1]) + " gold, and lose " + str(character[1]) + " life."
                 life -= character[1]
                 treasureCount += character[1]
-                print(life)
+                update()
+                nonInteraction()
+                exploreDisable()
                 if life <= 0:
                     header["text"] = "you are dead"
+                    youlose()
             else:
-                header["text"] = "you are dead"
+                header["text"] = "You are confronted by a minotaur."
+                youLose()
+
         else:
             if random.choice(outcomes):
                 header["text"] = "The " + character[0] + " is friendly, and gives you directions to the exit."
+                update()
+                nonInteraction()
+                exploreDisable()
             else:
                  header["text"] = "The " + character[0] + " gives you a healing potion. You heal 3 hp."
                  life += 3
+                 update()
+                 nonInteraction()
+                 exploreDisable()
 
-        update()
-        nonInteraction()
+        
             # add a variety of outcomes
+    # function to exit program       
+    def exit():
+        quit()
+    # function to fight npc's
+    def fight():
+        global life
+        global treasureCount
+        character = random.choice(enemies)
+        gain = random.choice(treasure)
+        if character[1] >= life:
+            header["text"] = "You are bested by the" + character[0] + "."
+            youLose() 
+        else:
+            header["text"] = "You Atack into a "+ character[0] + " you slay the " + character[0] + " You lose " + str(character[1]) + " life and gain " + str(gain) + " treasure."
+            life -= character[1]
+            treasureCount += gain
+            update()
+            nonInteraction()
+            exploreDisable()
 
+       
+    # function to end game disables buttons 
+    def youLose():
+        header["text"] += "You have died. Luckily you can try again"
+        leaveButton.config(state=DISABLED)
+        exploreButton.config(state=DISABLED)
+        speakButton.config(state=DISABLED)
+        fightButton.config(state=DISABLED)
+    
 
+    #function for winning the game 
+    def youWin():
+        header["text"] = "Congratulations you have suprised the cave of wonders. You found the Exit. Thanks for playing. "
+        leaveButton.config(state=DISABLED)
+        exploreButton.config(state=DISABLED)
+        speakButton.config(state=DISABLED)
+        fightButton.config(state=DISABLED)
+    
+    #function to disable explore button after its used once in each room 
+    def exploreDisable():
+         exploreButton.config(state=DISABLED)
+
+    # function to move forward thru the game 
     def roomChange():
         global life
 
         global room
+        exploreButton.config(state=NORMAL)
+        adjective = random.choice(roomAdjectives)
         room = random.choice(rooms)
-        messages = ["You find yourself in a dimly lit " + room[0] +  ", The room smells musty to your left you see an opening with a well lit pathway, to your right you see another opening with a dimly lit path.", "You enter a dark " + room[0] + ", The Room smells musty and is damp. you barely make out 2 paths one to the left and one to the right." ]
+        messages = ["You find yourself in a " + adjective + " " + room[0] +  "." ]
         # trigger trap here
-        if random.randint(1,8) != 7:
-            msg = random.choice(messages)
-            header["text"] = msg
+        if treasureCount >= 75:
+            # function for final room 
+            youWin()
         else:
-            header["text"] = traps[0]
-        update()
-    
+            if random.randint(1,8) != 7:
+                msg = random.choice(messages)
+                header["text"] = msg
+                update()
+            else:
+                header["text"] = random.choice(traps)
+                youLose()
+            
+       
+    # function updates totals for treasure and life 
     def update():
         lifeLabel["text"] = "Life: " + str(life)
-        goldLabel["text"] = "Gold: " + str(treasureCount)
-
-        
-
-
-    win = Toplevel()
-    win.title("My RPG")
-    messageFrame = Frame(win, bg="black",border=20)
-    messageFrame.grid(row=0,column=0, columnspan=4, padx=10,pady=10)
-    header = Label(messageFrame, justify="center",  wraplength=150, text="A weary traveller you see a cave in the storm and head toward it. As you approach the cave you see a small light within the opening. Ypu call out but recieve no response. You Continue into the cave opening", bg="black", fg="white")
-    header.grid(row=0, column=0,columnspan=3)
-    exploreButton = Button(win,text="explore", padx=10, pady=10, command=explore)
-    fightButton = Button(win,text="fight", padx=10, pady=10, state=DISABLED)
-    speakButton = Button(win,text="speak", padx=10, pady=10, command=speak , state=DISABLED)
-    leaveButton = Button(win,text="Leave Room", padx=10, pady=10, command=roomChange)
+        goldLabel["text"] = "Tresure: " + str(treasureCount)
     
+    
+            
+    # variable to display correct character  
+    imageFile = userClass + ".jpg"
+# window creation 
+    root = Tk()
+    win = ttk.Frame(root )
+    win.grid(row=0,column=0,sticky="")
+    win.grid_rowconfigure(0, weight=1)
+    win.grid_columnconfigure(0, weight=1)
+    root.title( "My Rpg")
+    # import for character image
+    userImage = Image.open(imageFile)
+    userImageresized = userImage.resize((80,80), Image.ANTIALIAS)
+    display = ImageTk.PhotoImage( userImageresized)
+    # image placement 
+    userLabel = Label(win, image=display, padx=50)
+    userLabel.image = display
+    # screen message and button creation
+    nameLabel = Label(win,text=name )
+    messageFrame = Frame(win, bg="black",border=20)
+    header = Label(messageFrame, justify="center",  wraplength=150, text=("Welcome " + name + " You find yourself in the cave of wonders. The only way to escape is to find 75 treasures. Good Luck You will need it"), bg="black", fg="white")
+    exploreButton = Button(win,text="explore", padx=10, pady=10, command=explore)
+    fightButton = Button(win,text="fight", padx=10, pady=10, command=fight, state=DISABLED)
+    speakButton = Button(win,text="speak", padx=10, pady=10, command=speak , state=DISABLED)
+    leaveButton = Button(win,text="Move Forward", padx=10, pady=10, command=roomChange)
+     # message and button placment 
+    messageFrame.grid(row=0,column=2, columnspan=3, padx=10,pady=10)
+    nameLabel.grid(column=0, row=2, columnspan=2)
+    userLabel.grid(row=0,column=0, columnspan=2)
+    header.grid(row=0, column=0,columnspan=3)
+    
+    #life label and a spacer creation
+    buttonSpacer = Label(win, text="")
+    exitButton = Button(win, text="exit", bg="red",padx=10, pady=10, fg="white", command=exit)
     lifeLabel = Label(win, text="Life: " + str(life))
-    goldLabel = Label(win, text="Gold: " + str(treasureCount))
-
+    goldLabel = Label(win, text="Treasure: " + str(treasureCount))
+    buttonSpacer.grid(row=5, column=0, columnspan=3)
+   
+   # Life an treasure label placment 
     lifeLabel.grid(row=3, column=1, pady=20)
     goldLabel.grid(row=3, column=2)
 
    
-
+# button placment 
     exploreButton.grid(row=4, column=0)
     fightButton.grid(row=4, column=1)
     speakButton.grid(row=4, column=2)
     leaveButton.grid(row=4, column=3)
+    exitButton.grid(row=7, column=3)
+
+    win.mainloop()
--- Rpg/project.py
+++ Rpg/project.py
@@ -1,52 +1,73 @@
 from tkinter import *
 from tkinter import ttk
 from explore import gameplay
+from PIL import Image, ImageTk
 import random 
-
-root = Tk()
-frm = ttk.Frame(root )
-frm.grid(row=0,column=0,sticky="")
-frm.grid_rowconfigure(0, weight=1)
-frm.grid_columnconfigure(0, weight=1)
-root.title( "My Rpg")
-root.geometry("700x350")
-global username 
-
- 
-
-def characterCreation(x):
-    username = nameEntry.get()
-    print(username)
-    userClass= x
-    gameplay(username,userClass )
+# This is the main page of the game, On this screen the user creates a user name and chooses a class. The user name and class are passed to the next screen to be used.
+#Main Function
+def MainGame():
+    #set up for window 
+    root = Tk()
+    frm = ttk.Frame(root )
+    frm.grid(row=0,column=0,sticky="")
+    frm.grid_rowconfigure(0, weight=1)
+    frm.grid_columnconfigure(0, weight=1)
+    root.title( "The Cave of Wonders")
+    root.geometry("500x500")
+    global username 
 
 
 
-
-
-myLabel = Label(frm,text="Welcome to my rpg", font=("Arial",  30), padx=10, pady=10)
-classChoiceLabel = Label(frm,text = "Please Choose a Class", font=15, padx=10,pady=10)
-myButton = Button(frm,text="paladin", padx=10, pady=10, command=lambda: characterCreation("paladin"))
-myButton2 = Button(frm,text="wizard", padx=10, pady=10, command=lambda: characterCreation("wizard"))
-myButton3 = Button(frm,text="warrior", padx=10, pady=10, command=lambda: characterCreation("warrior"))
-myButton4 = Button(frm,text="Thief", padx=10, pady=10, command=lambda: characterCreation("thief"))
-nameEntry = Entry(frm, width=25)
-nameEntry.get()
-nameLabel = Label(frm, text="Enter your characters name")
-
-myLabel.grid(row=1 , column=0, columnspan=4)
-
-classChoiceLabel.grid(row=3, column= 0, columnspan=4)
-
-nameEntry.grid(row=2, column=2, columnspan=3)
-nameLabel.grid(row=2, column=0, columnspan=2)
-
-myButton.grid(row=4, column=0)
-myButton2.grid(row=4, column=1)
-myButton3.grid(row=4, column=2)
-myButton4.grid(row=4, column=3)
+# image at bottom of screen 
+    pillow_image = Image.open("background.jpg")
+    test = ImageTk.PhotoImage( pillow_image)
+    label1 = Label( image=test, padx=50)
+    label1.image = test
+    label1.grid(row=6,column=0)
+    
+    #function to verify username entere and to pass name and class choice 
+    def characterCreation(x):
+        username = nameEntry.get()
+        if username:
+            print(username)
+            userClass= x
+            root.destroy()
+            gameplay(username,userClass )
+        else:
+            errorLabel["text"] = "You failed to enter a username"
 
 
 
 
-root.mainloop()
+
+# label and buttons for screen 
+    myLabel = Label(frm,text="The Cave of Wonders", font=("Arial",  30), padx=10, pady=10)
+    classChoiceLabel = Label(frm,text = "Please Choose a Class", font=15, padx=10,pady=10)
+    myButton = Button(frm,text="paladin", padx=10, pady=10, command=lambda: characterCreation("paladin"))
+    myButton2 = Button(frm,text="wizard", padx=10, pady=10, command=lambda: characterCreation("wizard"))
+    myButton3 = Button(frm,text="warrior", padx=10, pady=10, command=lambda: characterCreation("warrior"))
+    myButton4 = Button(frm,text="Thief", padx=10, pady=10, command=lambda: characterCreation("thief"))
+    nameEntry = Entry(frm, width=25)
+    nameEntry.get()
+    nameLabel = Label(frm, text="Enter your characters name")
+    errorLabel = Label(frm, text="", fg="red")
+
+# placment of label and buttons 
+    myLabel.grid(row=1 , column=0, columnspan=4)
+    errorLabel.grid(row=3, column=2, columnspan=3)
+    classChoiceLabel.grid(row=4, column= 0, columnspan=4)
+
+    nameEntry.grid(row=2, column=2, columnspan=3)
+    nameLabel.grid(row=2, column=0, columnspan=2)
+
+    myButton.grid(row=5, column=0)
+    myButton2.grid(row=5, column=1)
+    myButton3.grid(row=5, column=2)
+    myButton4.grid(row=5, column=3)
+
+
+
+
+    root.mainloop()
+
+MainGame()
